9b6a47c13c8ccd0c63f92cd565c5e9c2eb013338
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-02-21 14:53:25 +0100

    Small redesign the button scale

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -18,7 +18,7 @@ export default class Question extends PureComponent {
         const { capability, respondent } = this.props
         const { response } = this.state
         const note = event.target.value
-        
+
         this.setState({ isSavingAnswer: true })
 
         const api = new API()
@@ -43,32 +43,31 @@ export default class Question extends PureComponent {
                         <h2>{capability.name}</h2>
                     </div>
                     <div className="available-answers">
-                        <div className="answer-option">
+                        <div className="answer-option fully disagree">
                             <input type="radio" id={"question-" + id + "-level-0"} name={"question-" + id} value="0" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-0"}></label>
                         </div>
-                        <div className="answer-option">
+                        <div className="answer-option relatively disagree">
                             <input type="radio" id={"question-" + id + "-level-1"} name={"question-" + id} value="1" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-1"}></label>
                         </div>
-                        <div className="answer-option">
+                        <div className="answer-option slightly disagree">
                             <input type="radio" id={"question-" + id + "-level-2"} name={"question-" + id} value="2" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-2"}></label>
                         </div>
-                        <div className="answer-option">
+                        <div className="answer-option neutral">
                             <input type="radio" id={"question-" + id + "-level-3"} name={"question-" + id} value="3" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-3"}></label>
                         </div>
-                        <div className="answer-option">
-
+                        <div className="answer-option slightly agree">
                             <input type="radio" id={"question-" + id + "-level-4"} name={"question-" + id} value="4" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-4"}></label>
                         </div>
-                        <div className="answer-option">
+                        <div className="answer-option relatively agree">
                             <input type="radio" id={"question-" + id + "-level-5"} name={"question-" + id} value="5" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-5"}></label>
                         </div>
-                        <div className="answer-option">
+                        <div className="answer-option fully agree">
                             <input type="radio" id={"question-" + id + "-level-6"} name={"question-" + id} value="6" onChange={this.handleOptionChange} />
                             <label htmlFor={"question-" + id + "-level-6"}></label>
                         </div>
